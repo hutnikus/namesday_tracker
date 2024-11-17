ba1e755b8ba5a99fd538ac3a07f6e25c33999e78
hutnikus <hutnand@gmail.com>
2024-11-17 18:38:22 +0100

    Set up github action for build apk

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,6 +45,22 @@ android {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
         }
     }
+    signingConfigs {
+        create("release") {
+            storeFile = file(System.getenv("KEYSTORE_FILE") ?: " ")
+            storePassword = System.getenv("KEYSTORE_PASSWORD") ?: " "
+            keyAlias = System.getenv("KEY_ALIAS") ?: " "
+            keyPassword = System.getenv("KEY_PASSWORD") ?: " "
+        }
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = true
+            isShrinkResources = true
+            signingConfig = signingConfigs.getByName("release")
+        }
+    }
 }
 
 dependencies {
