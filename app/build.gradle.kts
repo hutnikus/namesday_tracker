34bb5334378624edc1c16a67a59bd4ddf44f10d7
hutnikus <hutnand@gmail.com>
2024-11-23 23:54:02 +0100

    Change compile sdk (for CI/CD)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.namesdaytracker"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.namesdaytracker"
